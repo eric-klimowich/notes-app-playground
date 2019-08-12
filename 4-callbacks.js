df213abea986c3b5ec8db959a0433a08961c6d92
eric-klimowich <eklimowich@hotmail.com>
2019-08-12 17:12:57 -0400

    Practiced with promises, and revisited callbacks to view side-by-side

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -1,3 +1,38 @@
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error.', undefined)
+        callback(undefined, [1, 4, 7])
+    }, 2000);
+}
+
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // setTimeout(() => {
 //     console.log('2 seconds are up!')
 // }, 2000)
@@ -33,12 +68,12 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
-const add = (a, b, callback) => {
-    setTimeout(() => {
-        callback(a + b)
-    }, 2000);
-}
+// const add = (a, b, callback) => {
+//     setTimeout(() => {
+//         callback(a + b)
+//     }, 2000);
+// }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-})
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
